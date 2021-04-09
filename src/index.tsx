527611d5a17a409cd91c8446396a475cd81eee04
dinesh-rapidops <dinesh.dabhi@rapidops.com>
2021-04-09 20:14:45 +0530

    Initial commit

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function IndexPage() {
-    // @ts-ignore
-    return (
-        <>
-            <div>{`Today's date is ${Date()}`}</div>
-        </>
-    );
-}
