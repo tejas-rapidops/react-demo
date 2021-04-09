7aedac4ce6b14e8260e6685d2e453306d869908a
dinesh-rapidops <dinesh.dabhi@rapidops.com>
2021-04-09 18:06:04 +0530

    Initial commit

---
+++ build/TodaysDate.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function TechnicianCallDetails() {
+    const date = new Date().toISOString();
+    return (
+        <div>{{date}}
+        </div>
+
+);
+}
